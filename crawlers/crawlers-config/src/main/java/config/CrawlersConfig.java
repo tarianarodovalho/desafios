9370303ebaf3cff727896554cb0fc3410a67299c
Tariana Gomes Rodovalho <tariana.rodovalho@gmail.com>
2019-12-16 11:12:18 -0300

    Added token path

--- crawlers/crawlers-config/src/main/java/config/CrawlersConfig.java
+++ crawlers/crawlers-config/src/main/java/config/CrawlersConfig.java
@@ -10,15 +10,25 @@ public class CrawlersConfig {
 
     private final int MINIMUM_BOMBASTIC_SCORE = 5000;
     private final String TELEGRAM_BOT_USERNAME="idwallTarianaCrawlerBot";
-    private final String TELEGRAM_TOKEN = "932071395:AAF1oOQxnFGtIAl696uWHRceF5EhoqOANgg";
+    private final String TELEGRAM_TOKEN = telegramToken();
     private final String REDDIT_ENDPOINT = "https://old.reddit.com/r/";
 
     private SubRedditGenerator subRedditGenerator = new SubRedditGenerator();
     private SubRedditCrawlerImpl subRedditCrawler = new SubRedditCrawlerImpl(MINIMUM_BOMBASTIC_SCORE, REDDIT_ENDPOINT);
     private SubRedditFinder subRedditFinder = new SubRedditFinder(subRedditCrawler, subRedditGenerator);
-    public CrawlersController crawlersController = new CrawlersController(subRedditFinder);
+    private CrawlersController crawlersController = new CrawlersController(subRedditFinder);
 
     public TelegramBot TelegramBot() {
         return new TelegramBot(TELEGRAM_BOT_USERNAME, TELEGRAM_TOKEN, crawlersController);
+    }
+
+    public String telegramToken(){
+        File resourcesDirectory = new File("../crawlers-application/src/main/resources/token.txt");
+        Path path = Paths.get(resourcesDirectory.getAbsolutePath());
+        try (Stream<String> token = Files.lines(path)) {
+            return token;
+        } catch (IOException e){
+            System.out.println("Não foi possível encontrar o token do bot.");
+        }
     }
 }
